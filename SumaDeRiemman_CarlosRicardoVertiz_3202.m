19edd3cce4eefbea86a8376766d5b1989263ff2c
@RicardoVert <82452690+CarlosRicardoVertiz@users.noreply.github.com>
2021-05-15 21:33:39 -0500

    Update SumaDeRiemman_CarlosRicardoVertiz_3202.m

--- SumaDeRiemman_CarlosRicardoVertiz_3202.m
+++ SumaDeRiemman_CarlosRicardoVertiz_3202.m
@@ -1,27 +1,38 @@
-%Titulo : Sumas de Riemman
-% Descripción: Script para encontrar el area de la curva de la funcion: 
-%"x = x^2+2 [1,4]
-% Autor: Carlos Ricardo Vertiz carlosricardovertiz@gmail.com
-% Fecha: 30/04/2021
+%Materia: Calculo Integral
+%Nombre Del Alumno: Carlos Ricardo Vertiz
+%correo electronico: carlosricardovertiz@gmail.com
+%grupo: 3202
 
-clc
-  clear
- syms x;
- 
- f = x^2+2;
-  a = 1
-  b = 4
-  n = 3-0;
-  dx = (b-a)/n;
-  resultado = zeros(1,n);
-  i =1;
-  fs=subs(f,x,(a+i*dx));
-  fr = fs * dx;
-  resultado = fr;
-  for i=1:n
-    fs=subs(f,x,(a+1+dx));
-    fr=fs*dx;
-    resultado(i+1)=resultado(i)+fr
-   endfor 
-   fprintf('total es:',double(resultado));
-   
+% SUMAS DE RIEMMAN
+% Descripcion: grafica la funcion f(x)=(1)./(1-(x.^2),[-2,4] Â¿existe?
+
+% limpiar variables
+clear   
+% f(x)=(x.^2)+2,  [1,4]
+
+%variables que sirven para saber los limites en donde se sacara el area
+lim_a=1;
+lim_b=4;
+
+num_inter=200;
+
+%paso de la funcion
+f = inline ("x^2 + 2");
+%Calculo de h (o incremento de x)
+h= (lim_b-lim_a)/num_inter;
+% m+1 puntos, m intervalos
+x= lim_a:h:lim_b;
+int=0;
+%para cada intervalo
+for i=1:num_inter;
+  %extremos x(i), x(i+1)
+  xm=(x(i)+x(i+1))/2;
+  int=int+f(xm)*h;
+end 
+fprintf ("el area es: ")
+fprintf("%d", int);
+
+%Querido amigo, este script funciona para calcular el area por y encima de la curva
+%si mi explicacion es la correcta, no debe aver problema en que puedas interpretar
+%el script, espero y te sea de ayuda, fue gracias a la ayuda de nuestro profesor+
+%quien nos dio la oportunidad de guiarnos con su script. saludos!
